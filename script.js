6bf9654307f552b4d6696661388b3225f9c80a73
Finidi47 <finidisaid@gmail.com>
2025-06-19 11:26:34 +0300

    Create the addTask Function

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ document.addEventListener('DOMContentLoaded', function(){
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
+
+    // Create the addTask Function
+    const addTask = function(){
+        // Retrieve and trim the value from the task input field
+        const taskText = taskInput.value.trim();
+
+        // Alert the user to enter a task if taskText is empty
+        if (taskText === ""){
+            alert('Enter your task');
+            return; //exit the function if no valid input
+        }
+    }
 })

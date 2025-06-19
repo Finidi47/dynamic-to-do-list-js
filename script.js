beb7861189a5404971f9494bc71cc120e41df897
Finidi47 <finidisaid@gmail.com>
2025-06-19 23:46:43 +0300

    Task creation and removal

--- script.js
+++ script.js
@@ -15,5 +15,23 @@ document.addEventListener('DOMContentLoaded', function(){
             alert('Enter your task');
             return; //exit the function if no valid input
         }
+
+        // Task Creation and Removal
+            const newLi = document.createElement('li');
+            newLi.textContent = taskText;
+            const newButton = document.createElement('button');
+            newButton.textContent = 'Remove';
+            newButton.classList.add('remove-btn');
+            newButton.onclick = () =>{
+                taskList.removeChild(newLi);
+            }
+        
+            newLi.appendChild(newButton);
+            taskList.appendChild(newLi);
+
+            taskInput.value = "";
+        
     }
+
+    addButton.addEventListener('click', addTask);
 })
